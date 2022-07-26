073e96eefe9fd3a3824b770b2fdb0d1a6668265d
c7021100 <christoph.angermann@uibk.ac.at>
2022-07-26 16:22:51 +0200

    implemented 3d model

--- inpainting_masks.py
+++ inpainting_masks.py
@@ -9,48 +9,46 @@ from matplotlib import cm
 import matplotlib.colors as colors
 from scipy import ndimage, misc
 
-N = 256
-f = np.zeros([N, N, N])
+def define_random_mask(N=256, prop = 0.005):
+    f = np.zeros([N, N, N])
+    a = N*.5
+    x = np.linspace(-N/2, N/2-1, N)
+    X,Y,Z = np.meshgrid(x,x,x)
+    
+    
+    while True:
+        aa = a*np.random.rand(1)
+        bb = a*np.random.rand(1)
+        cc = a*np.random.rand(1)
+    
+        X_shift = X - np.sign(0.5 - np.random.rand(1))*N/2*np.random.rand(1)
+        Y_shift = Y - np.sign(0.5 - np.random.rand(1))*N/2*np.random.rand(1)
+        Z_shift = Z - np.sign(0.5 - np.random.rand(1))*N/2*np.random.rand(1)
+    
+        idx = (X_shift**2/aa**2 + Y_shift**2/bb**2 + Z_shift**2/bb**2) <= 1
+        # angle = np.random.randint(1, 90+1)
+        # idx = ndimage.rotate(idx, angle, reshape = False)
+        idx = np.rot90(idx,k=np.random.choice([1,2,3]))
+        f[idx] = 1
+    
+        fflat = f.flatten()
+    
+        if np.count_nonzero(fflat)/np.prod(f.shape) >= prop:
+            break
+    return f.astype(np.uint8)
 
-prop = 0.0001
-x = np.linspace(-N/2, N/2-1, N)
-X,Y,Z = np.meshgrid(x,x,x)
+# f = define_random_mask()
 
-a = 25
-b = 10
-c = 40
+# s1 = np.squeeze(np.sum(f, 0))
+# s2 = np.squeeze(np.sum(f, 1))
+# s3 = np.squeeze(np.sum(f, 2))
 
-while True:
-    aa = a*np.random.rand(1)
-    bb = b*np.random.rand(1)
-    cc = c*np.random.rand(1)
+# s1[s1!=0] = 1
+# s2[s2!=0] = 1
+# s3[s3!=0] = 1
 
-    X_shift = X - np.sign(0.5 - np.random.rand(1))*N*np.random.rand(1)
-    Y_shift = Y - np.sign(0.5 - np.random.rand(1))*N*np.random.rand(1)
-    Z_shift = Z - np.sign(0.5 - np.random.rand(1))*N*np.random.rand(1)
-
-    idx = (X_shift**2/aa**2 + Y_shift**2/bb**2 + Z_shift**2/bb**2) <= 1
-    angle = np.random.randint(1, 90+1)
-    idx = ndimage.rotate(idx, angle, reshape = False)
-   
-    f[idx] = 1
-
-    fflat = f.flatten()
-
-    if np.count_nonzero(fflat)/np.prod(f.shape) >= prop:
-        break
-
-
-s1 = np.squeeze(np.sum(f, 0))
-s2 = np.squeeze(np.sum(f, 1))
-s3 = np.squeeze(np.sum(f, 2))
-
-s1[s1!=0] = 1
-s2[s2!=0] = 1
-s3[s3!=0] = 1
-
-fig, (ax1, ax2, ax3) = plt.subplots(1, 3)
-ax1.imshow(s1)
-ax2.imshow(s2)
-ax3.imshow(s3)
-plt.show()
+# fig, (ax1, ax2, ax3) = plt.subplots(1, 3)
+# ax1.imshow(s1)
+# ax2.imshow(s2)
+# ax3.imshow(s3)
+# plt.show()
--- model_v2.py
+++ model_v2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Tue Jul 19 11:02:58 2022
+Created on Tue Jul 26 14:30:11 2022
 
 @author: c
 """
@@ -25,6 +25,7 @@ import matplotlib.pyplot as plt
 from tqdm import tqdm as tq
 from scipy.ndimage import rotate
 import time
+from inpainting_masks import define_random_mask
 
 
 torch.cuda.empty_cache()
@@ -35,23 +36,34 @@ train_on_gpu = torch.cuda.is_available()
 
 def NORM(out_f,normalization):
     normlayer = nn.ModuleDict([
-        ['instance',nn.InstanceNorm2d(out_f)],
-        ['batch', nn.BatchNorm2d(out_f)]
+        ['instance',nn.InstanceNorm3d(out_f)],
+        ['batch', nn.BatchNorm3d(out_f)]
         ])
     return normlayer[normalization]
 
+class Conv3dsep(nn.Module):
+    def __init__(self, in_f, out_f, stride, padding):
+        super(Conv3dsep,self).__init__()
+        
+        self.layer  = nn.Sequential(
+            nn.Conv3d(in_f,  in_f, kernel_size=3,stride=stride,padding=padding,groups=in_f),
+            nn.Conv3d(in_f,  out_f,kernel_size=1,stride=1,padding='same'))
+    
+    def forward(self, x):
+        return (self.layer(x))
+    
 
 class double_conv(nn.Module):
     def __init__(self,in_f,out_f,normalization = 'instance'):
         super(double_conv, self).__init__()
         
         self.conv1  = nn.Sequential(
-            nn.Conv2d(in_f,  out_f,kernel_size=3,stride=1,padding='same'),
+            Conv3dsep(in_f, out_f,stride=1,padding='same'),           
             NORM(out_f, normalization),
             nn.ReLU())
         
         self.conv2  = nn.Sequential(
-            nn.Conv2d(out_f,  out_f,kernel_size=3,stride=1,padding='same'),
+            Conv3dsep(out_f, out_f,stride=1,padding='same'),   
             NORM(out_f, normalization),
             nn.ReLU())
                  
@@ -64,7 +76,7 @@ class downsampling(nn.Module):
         super(downsampling, self).__init__()
         
         self.down  = nn.Sequential(
-            nn.Conv2d(in_f,  out_f,kernel_size=3,stride=2,padding=(1,1)),
+            Conv3dsep(in_f, out_f,stride=2,padding=(1,1,1)),   
             NORM(out_f,normalization),
             nn.ReLU())
                  
@@ -73,21 +85,21 @@ class downsampling(nn.Module):
     
     
 class upsampling(nn.Module):
-    def __init__(self,in_f,out_f,normalization='instance',bilinear=False):
+    def __init__(self,in_f,out_f,normalization='instance',nearest=False):
         super(upsampling, self).__init__()
         
-        if bilinear:
-            self.up = nn.Upsample(scale_factor=2, mode = 'bilinear',align_corners=True)
+        if nearest:
+            self.up = nn.Upsample(scale_factor=2, mode = 'nearest')
         else:
-            self.up = nn.ConvTranspose2d(in_f,out_f,kernel_size=4,stride=2,padding=(1,1))
+            self.up = nn.ConvTranspose3d(in_f,out_f,kernel_size=4,stride=2,padding=(1,1,1))
             
         self.conv1  = nn.Sequential(
-            nn.Conv2d(in_f, out_f,kernel_size=3,stride=1,padding='same'),
+            Conv3dsep(in_f, out_f,stride=1,padding='same'),   
             NORM(out_f, normalization),
             nn.LeakyReLU(0.2))
         
         self.conv2  = nn.Sequential(
-            nn.Conv2d(out_f,  out_f,kernel_size=3,stride=1,padding='same'),
+            Conv3dsep(out_f, out_f,stride=1,padding='same'),   
             NORM(out_f, normalization),
             nn.LeakyReLU(0.2))
             
@@ -100,7 +112,7 @@ class upsampling(nn.Module):
 
 class unet(nn.Module):
     def __init__(self, n_channels, f_size, normalization='instance',
-                 out_acti = 'relu'):
+                 out_acti = 'sigmoid'):
         super(unet, self).__init__()
         self.dc0 = double_conv(n_channels, f_size, normalization)
         self.ds0 = downsampling(f_size, f_size, normalization)
@@ -118,19 +130,17 @@ class unet(nn.Module):
         self.up_out = upsampling(2*f_size, f_size, normalization)
         self.up_unc = upsampling(2*f_size, f_size, normalization)
         self.out = nn.Sequential(
-            nn.Conv2d(f_size, 1, kernel_size=3, stride=1, padding='same'),
+            Conv3dsep(f_size, 1,stride=1,padding='same'),   
             nn.ModuleDict([['relu',nn.ReLU()],['sigmoid',nn.Sigmoid()]]
                           )[out_acti])
         self.unc = nn.Sequential(
-            nn.Conv2d(f_size, 1, kernel_size=3, stride=1, padding='same'),
+            Conv3dsep(f_size, 1,stride=1,padding='same'),   
             nn.Softplus())
         
         
     def forward(self, x):
         x = x.float()
-        d = x.shape[0]
-        x = torch.cat([x[:,0],x[:,1]],dim = 0);
-        # print(x.shape)
+      
         skip0 = self.dc0(x)
         down1 = self.ds0(skip0)
         skip1 = self.dc1(down1)
@@ -138,90 +148,43 @@ class unet(nn.Module):
         skip2 = self.dc2(down2)
         down3 = self.ds2(skip2)
         skip3 = self.dc3(down3)
-        down4 = self.ds3(skip3)
-        skip4 = self.dc4(down4)
+        # down4 = self.ds3(skip3)
+        # skip4 = self.dc4(down4)
         
-        upsa3 = self.up3(skip4,skip3)
-        upsa2 = self.up2(upsa3,skip2)
+        # upsa3 = self.up3(skip4,skip3)
+        upsa2 = self.up2(skip3,skip2)
         upsa1 = self.up1(upsa2,skip1)
         
         obranch1 = self.up_out(upsa1,skip0)
         img_out = self.out(obranch1)
-        img_out = torch.stack(torch.split(img_out,[d,d],dim=0),dim=1)
         
         obranch2 = self.up_unc(upsa1,skip0)
         unc_out = self.unc(obranch2)
-        unc_out = torch.stack(torch.split(unc_out,[d,d],dim=0),dim=1)
         
         return([img_out,unc_out])
       
     
-class l1dist(nn.Module):
-    __name__ = 'dice_loss'
-    
-    def __init__(self,uncertainty=True,eps=1e-6):
-        self.uncertainty = uncertainty
-        self.eps = eps
-        super().__init__()
+class DiceLoss(nn.Module):
+    def __init__(self, weight=None, size_average=True):
+        super(DiceLoss, self).__init__()
+
+    def forward(self, inputs, targets, eps=1e-7):
+                
+        #flatten label and prediction tensors
+        inputs = inputs[0].view(-1)
+        targets = targets.view(-1)
         
-    def forward(self,pred, gt):
-        p = pred[0]; 
-        if self.uncertainty:
-            u = torch.clip(pred[1],self.eps,1e8)
-            t1 = torch.abs(p-gt)/u
-            t1 = torch.where(t1<=2,t1,2+torch.log(torch.clip(t1-2+1,self.eps,1e8)))
-            return torch.mean(t1+torch.log(1.+2.*u))
-        else:
-            return torch.mean(torch.abs(p-gt))
+        intersection = (inputs * targets).sum()                            
+        dice = (2.*intersection + eps)/(inputs.sum() + targets.sum() + eps)  
+        
+        return 1 - dice
+
 
         
 #%%
 
 
 data_path = 'data/%s'%task
-
-
-def intensity_perturbation_2d(x):
-    m = x>0; 
-    i,j = np.random.choice(range(x.shape[0]), size=2)
-    while(m[i,j]==0):
-        i,j = np.random.choice(range(x.shape[0]), size=2)
-    size = int(np.random.choice(np.linspace(.1,.5,100))*np.min([i,j]))
-    x[i-size:i+size,j-size:j+size] *= .5
-    return x
-
-def inpaint_3d(x):
-    s = x.shape[0]
-    m = x>0.02; 
-    i,j,k = np.random.choice(range(s), size=3)
-    while(m[i,j,k]==0):
-        i,j,k = np.random.choice(range(s), size=3)
-    size = int(np.random.choice(np.linspace(12,np.max(
-        [12,np.min([i,s-i,j,s-j,k,s-k])]),100)))
-    x[i-size:i+size,j-size:j+size,k-size:k+size] = np.random.choice(np.linspace(
-        np.min(x),np.max(x),100))
-    return x
-    
-
-    
-    
-def projection(data):
-    rand_angle = np.random.choice(range(180),2, replace=False)
-    res_per = []; res_ori = []
-    for ra in rand_angle:
-        rot = rotate(data, angle=ra, axes=(0,2), reshape=False)
-        ori = rot.copy(); per = rot.copy()
-        
-        ori = np.sum(ori,axis=0)
-        ori /= np.quantile(ori,.98); ori[ori>1]=1
-        res_ori.append(np.expand_dims(ori,0))
-        
-        per = inpaint_3d(per)
-        per = np.sum(per,axis=0)
-        per /= np.quantile(per,.98); per[per>1]=1
-        res_per.append(np.expand_dims(per,0))
-
-    return [np.stack(res_per,axis=0), np.stack(res_ori,axis=0)]
 
 class MedDataset(Dataset):
     def __init__(
@@ -243,16 +206,18 @@ class MedDataset(Dataset):
         image_name = self.img_ids[idx]
         image_path = os.path.join(self.img_folder , image_name)
         nifti = nib.load(image_path)
-        data = nifti.get_fdata()
-        per_data, ori_data = projection(data)
-        # data = np.expand_dims(data,1)
-        # per_data = generate_artifacts(data)
-        # matrix = nifti.affine
+        data = nifti.get_fdata().copy()[::2,::2,::2]
+        mask = define_random_mask(N=data.shape[0])
+        rand_int = np.random.choice(np.linspace(data.min(),data.max(),1000))
+        per_data = np.where(mask!=1,data,rand_int)
+        per_data = np.expand_dims(per_data,0)
+        per_data /= np.quantile(per_data,.98)
         if self.disturb_input:
-            return per_data, ori_data
+            return per_data, np.expand_dims(mask,0)
         else:
-            return ori_data, ori_data
-    
+            data = np.expand_dims(data,0)
+            return data, data
+
     def __len__(self):
         return(len(self.img_ids))
     
@@ -273,17 +238,17 @@ test_dataset = MedDataset(
 
 #%%
 
-criterion = l1dist(uncertainty = True)
-model = unet(n_channels =1, f_size=32)
+criterion = DiceLoss()
+model = unet(n_channels =1, f_size=16)
 if train_on_gpu:
     model.cuda()
-summary(model, (2,1,256,256))
+# summary(model, (1,128,128,128))
 optimizer = torch.optim.Adam(model.parameters(), lr=1e-4)
 scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, factor=0.5, patience=5, cooldown=2)
 
 #%%
 
-batch_size = 4
+batch_size = 2
 
 train_loader = DataLoader(
     train_dataset, batch_size=batch_size, shuffle=True,
@@ -354,21 +319,21 @@ for epoch in range(1, n_epochs+1):
             bar.set_postfix(ordered_dict={"valid_loss":loss.item()})
     preds = output[0].cpu().detach().numpy()[:,0]
     uncer = output[1].cpu().detach().numpy()[:,0]
-    inp   = data.cpu().detach().numpy()[:,0]
+    gts   = target.cpu().detach().numpy()[:,0]
     
     
     fig, ax = plt.subplots(2,4,figsize=(12,5)); 
     for j in [0,1]:
-        im = ax[j,0].imshow(inp[j,0], cmap='Greys_r')
+        im = ax[j,0].imshow(np.sum(gts[j],0), cmap='Greys_r')
         ax[j,0].axis('off')
         plt.colorbar(im,ax=ax[j,0])
-        im = ax[j,1].imshow(preds[j,0], cmap='Greys_r')
+        im = ax[j,1].imshow(np.sum(preds[j],0), cmap='Greys_r')
         ax[j,1].axis('off')
         plt.colorbar(im,ax=ax[j,1])
-        im = ax[j,2].imshow(np.abs(inp[j,0]-preds[j,0]), cmap='gist_rainbow')
+        im = ax[j,2].imshow(np.abs(np.sum(gts[j]-preds[j],0)), cmap='gist_rainbow')
         ax[j,2].axis('off')
         plt.colorbar(im,ax=ax[j,2])
-        im = ax[j,3].imshow(uncer[j,0], cmap='gist_rainbow')
+        im = ax[j,3].imshow(np.sum(uncer[j],0), cmap='gist_rainbow')
         ax[j,3].axis('off')
         plt.colorbar(im,ax=ax[j,3])
         
@@ -416,22 +381,18 @@ for epoch in range(1, n_epochs+1):
     gts = np.concatenate(gts,0)
     uncers = np.concatenate(uncers,0)
 
-    fig, ax = plt.subplots(4,4,figsize=(12,10)); 
-    for j in [0,1,2,3]:
-        
-        im = ax[j,0].imshow(gts[j,0], cmap='Greys_r')
+    fig, ax = plt.subplots(2,4,figsize=(12,5)); 
+    for j in [0,1]:
+        im = ax[j,0].imshow(np.sum(gts[j],0), cmap='Greys_r')
         ax[j,0].axis('off')
         plt.colorbar(im,ax=ax[j,0])
-        
-        im = ax[j,1].imshow(preds[j,0], cmap='Greys_r')
+        im = ax[j,1].imshow(np.sum(preds[j],0), cmap='Greys_r')
         ax[j,1].axis('off')
         plt.colorbar(im,ax=ax[j,1])
-        
-        im = ax[j,2].imshow(np.abs(gts[j,0]-preds[j,0]), cmap='gist_rainbow')
+        im = ax[j,2].imshow(np.abs(np.sum(gts[j]-preds[j],0)), cmap='gist_rainbow')
         ax[j,2].axis('off')
         plt.colorbar(im,ax=ax[j,2])
-        
-        im = ax[j,3].imshow(uncers[j,0], cmap='gist_rainbow')
+        im = ax[j,3].imshow(np.sum(uncers[j],0), cmap='gist_rainbow')
         ax[j,3].axis('off')
         plt.colorbar(im,ax=ax[j,3])
         
