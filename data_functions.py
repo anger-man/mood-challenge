7272c92a820c3df5cf2c85321acbf3d3dd12b120
c7021100 <christoph.angermann@uibk.ac.at>
2022-08-03 12:35:37 +0200

    updated data functions

--- data_functions.py
+++ data_functions.py
@@ -23,7 +23,7 @@ from tqdm import tqdm as tq
 from scipy.ndimage import rotate
 import time
 from skimage.measure import block_reduce
-
+from skimage.filters import sobel
 
 
     
@@ -35,7 +35,7 @@ class DiceLoss(nn.Module):
     def __init__(self, weight=None, size_average=True):
         super(DiceLoss, self).__init__()
 
-    def forward(self, inputs, targets, eps=1e-7):
+    def forward(self, inputs, targets, eps=1e-6):
                 
         inputs = inputs[0].view(-1) #equal to np.reshape(-1)
         targets = targets.view(-1)  #equal to np.reshape(-1)
@@ -67,7 +67,7 @@ def add_gaussian_noise(data, rand_mask):
     Disturb the voxel values of data with Gaussian noise wrt to 
     the random mask
     """
-    scale=np.random.uniform(.05,.1)
+    scale=np.random.uniform(.07,.14)
     noisy_sample = data + np.random.normal(loc=0, scale=scale,
                                            size=data.shape)
     per_data = np.where(rand_mask!=1,data,noisy_sample)
@@ -80,9 +80,20 @@ def shift_intensity(data, rand_mask):
     Shift the intensity of voxel values up to +-0.5 wrt to 
     the random mask
     """
-    shift = np.random.rand()-.5
+    shift = np.random.choice([-.5,-.4,-.3,.3,.4,.5])
     shifted = shift+data
     per_data = np.where(rand_mask!=1,data,shifted)
+    return per_data
+
+###############################################################################
+
+def apply_sobel_filter(data, rand_mask):
+    """
+    Apply the sobel filter to the regions indicated by  
+    the random mask
+    """
+    sobel_data = sobel(data)
+    per_data = np.where(rand_mask!=1,data,sobel_data)
     return per_data
 
 #%%
@@ -90,7 +101,6 @@ def shift_intensity(data, rand_mask):
 def generate_random_mask(
     dim: int=64, 
     propval: float=.1,
-    diameter: float=.25,
     shapes: list=['elliptical','cuboid'],
     shape_prop: list=[.5,.5]):
 
@@ -103,56 +113,59 @@ def generate_random_mask(
 
     prop=np.random.uniform(0,propval),
     f = np.zeros([dim, dim, dim])
+    diameter = 48/dim
     a = dim*diameter
     x = np.linspace(-dim/2, dim/2-1, dim)
     X,Y,Z = np.meshgrid(x,x,x)
     
-    
-    while True:
+    generate_components = np.random.rand()
+    if generate_components>.2:
         
-        s_prop = np.random.uniform(0,np.sum(shape_prop))
-        if s_prop < shape_prop[0]:
-            shape = shapes[0]
-        else:
-            shape = shapes[1]
+        for dummy in range(4):
             
-        if shape=='elliptical':
-            aa = a*np.random.rand(1)
-            bb = a*np.random.rand(1)
-            cc = a*np.random.rand(1)
-        
-            X_shift = X - np.sign(0.5 - np.random.rand(1))*dim/2*np.random.rand(1)
-            Y_shift = Y - np.sign(0.5 - np.random.rand(1))*dim/2*np.random.rand(1)
-            Z_shift = Z - np.sign(0.5 - np.random.rand(1))*dim/2*np.random.rand(1)
-        
-            idx = (X_shift**2/aa**2 + Y_shift**2/bb**2 + Z_shift**2/cc**2) <= 1
-            # angle = np.random.randint(1, 90+1)
-            # idx = ndimage.rotate(idx, angle, reshape = False)
-            idx = np.rot90(idx,k=np.random.choice([1,2,3]))
-            f[idx] = 1
-            fflat = f.flatten()
+            s_prop = np.random.uniform(0,np.sum(shape_prop))
+            if s_prop < shape_prop[0]:
+                shape = shapes[0]
+            else:
+                shape = shapes[1]
+                
+            if shape=='elliptical':
+                aa = a*np.random.rand(1)
+                bb = a*np.random.rand(1)
+                cc = a*np.random.rand(1)
             
-            if np.count_nonzero(fflat)/np.prod(f.shape) >= prop:
-                break
+                X_shift = X - np.sign(0.5 - np.random.rand(1))*dim/2*np.random.rand(1)
+                Y_shift = Y - np.sign(0.5 - np.random.rand(1))*dim/2*np.random.rand(1)
+                Z_shift = Z - np.sign(0.5 - np.random.rand(1))*dim/2*np.random.rand(1)
             
-        elif shape=='cuboid':
-            aa = int(.7*a*np.random.rand(1))
-            bb = int(.7*a*np.random.rand(1))
-            cc = int(.7*a*np.random.rand(1))
-        
-            x_mid = int(dim/2 - np.sign(0.5 - np.random.rand(1))*dim/2*np.random.rand(1))
-            y_mid = int(dim/2 - np.sign(0.5 - np.random.rand(1))*dim/2*np.random.rand(1))
-            z_mid = int(dim/2 - np.sign(0.5 - np.random.rand(1))*dim/2*np.random.rand(1))
-        
-            f[x_mid-aa:x_mid+aa,y_mid-bb:y_mid+bb,z_mid-cc:z_mid+cc] = 1
-            fflat = f.flatten()
+                idx = (X_shift**2/aa**2 + Y_shift**2/bb**2 + Z_shift**2/cc**2) <= 1
+                # angle = np.random.randint(1, 90+1)
+                # idx = ndimage.rotate(idx, angle, reshape = False)
+                idx = np.rot90(idx,k=np.random.choice([1,2,3]))
+                f[idx] = 1
+                fflat = f.flatten()
+                
+                if np.count_nonzero(fflat)/np.prod(f.shape) >= prop:
+                    break
+                
+            elif shape=='cuboid':
+                aa = int(.5*a*np.random.rand(1))
+                bb = int(.5*a*np.random.rand(1))
+                cc = int(.5*a*np.random.rand(1))
             
-            if np.count_nonzero(fflat)/np.prod(f.shape) >= prop:
-                break
+                x_mid = int(dim/2 - np.sign(0.5 - np.random.rand(1))*dim/2*np.random.rand(1))
+                y_mid = int(dim/2 - np.sign(0.5 - np.random.rand(1))*dim/2*np.random.rand(1))
+                z_mid = int(dim/2 - np.sign(0.5 - np.random.rand(1))*dim/2*np.random.rand(1))
             
-        else:
-            print('No valid shape'); pass;
-            
+                f[x_mid-aa:x_mid+aa,y_mid-bb:y_mid+bb,z_mid-cc:z_mid+cc] = 1
+                fflat = f.flatten()
+                
+                if np.count_nonzero(fflat)/np.prod(f.shape) >= prop:
+                    break
+                
+            else:
+                print('No valid shape'); pass;
+                
     return f.astype(np.uint8)
         
 #%%
@@ -181,6 +194,7 @@ class MedicalDataset(Dataset):
             task: str = 'brain',
             path: str = None,
             img_ids: np.array = None,
+            reduce_dim: bool = True,
             disturb_input = True,
             affine_matrix = False
         ):
@@ -190,6 +204,7 @@ class MedicalDataset(Dataset):
             else:
                 self.img_folder  = f"{path}/toy"
             self.img_ids = img_ids
+            self.reduce_dim = reduce_dim
             self.disturb_input = disturb_input
             self.affine_matrix = affine_matrix
             
@@ -205,12 +220,8 @@ class MedicalDataset(Dataset):
         
         #load the nifti file (affine matrix is omitted here)
         nifti = nib.load(image_path)
-        data = nifti.get_fdata() 
+        data = nifti.get_fdata()#[::2,::2,::2]
         aff_mat = nifti.affine
-        
-        #scale the data to [64,64,64]
-        fac = int(data.shape[0]/64)
-        data = block_reduce(data, (fac,fac,fac), np.mean)
         
         #define the random mask
         rand_mask = generate_random_mask(dim=data.shape[0],
@@ -222,11 +233,14 @@ class MedicalDataset(Dataset):
         # devide the entire scan by its 98% quantile
         data /= np.quantile(data,.98)
         
-        type_of_per = np.random.choice(['rand_int','gauss_noise','shift_int'])
+        type_of_per = np.random.choice(['rand_int','gauss_noise','shift_int','sobel'])
         if type_of_per == 'rand_int':
             per_data = random_intensity(data,rand_mask)
         elif type_of_per == 'gauss_noise':
             per_data = add_gaussian_noise(data,rand_mask)
+        elif type_of_per == 'sobel':
+            rand_mask[data==0] = 0
+            per_data = apply_sobel_filter(data,rand_mask)
         elif type_of_per == 'shift_int':
             per_data = shift_intensity(data,rand_mask)
         else:
@@ -235,12 +249,23 @@ class MedicalDataset(Dataset):
         #when evaluating on provided toy data, no perturbations are needed for 
         #the input instances, i.e. disturb_input is set to FALSE during testing
         if self.disturb_input:
+            
+            #scale the data to [64,64,64]
+            if self.reduce_dim:
+                fac = int(per_data.shape[0]/64)
+                per_data = block_reduce(per_data, (fac,fac,fac), np.mean)
+                rand_mask = block_reduce(rand_mask, (fac,fac,fac), np.median).round()
+                
             if self.affine_matrix:
                 return np.expand_dims(per_data,0), np.expand_dims(rand_mask,0),aff_mat
             else:
                 return np.expand_dims(per_data,0), np.expand_dims(rand_mask,0)
                 
         else:
+            if self.reduce_dim:
+                fac = int(data.shape[0]/64)
+                data = block_reduce(data, (fac,fac,fac), np.mean)    
+                
             if self.affine_matrix:
                 data = np.expand_dims(data,0)
                 return data, data, aff_mat
